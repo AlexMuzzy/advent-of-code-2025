8ff1e77a9f66520f66c7d2895c3b652b936bed16
AlexMuzzy <alexmuzzy33@gmail.com>
2025-12-01 23:51:37 +0000

    attempt on part 2

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ def get_file_input(day: int, part: int, is_sample: Optional[bool] = False):
 
 
 def main():
-    day1_part1_input = get_file_input(1, 1)
-    day1_part1_sample_input = get_file_input(1, 1, True)
-    print(f"Day 1 - part 1: {solve(day1_part1_input)}")
-    print(f"Day 1 - part 1 - sample: {solve(day1_part1_sample_input)}")
+    day1_input = get_file_input(1, 1)
+    day1_sample_input = get_file_input(1, 1, True)
+    print(f"Day 1 - part 1: {solve(day1_input)}")
+    print(f"Day 1 - part 1 - sample: {solve(day1_sample_input)}")
+
+    print(f"Day 1 - part 2: {solve(day1_input, True)}")
+    print(f"Day 1 - part 2 - sample: {solve(day1_sample_input, True)}")
 
 
 if __name__ == "__main__":
--- solutions/day1/solution.py
+++ solutions/day1/solution.py
@@ -1,44 +1,61 @@
-from typing import List
+import math
+from typing import List, Tuple
 
 
-def compute_left(amount: int, val: int) -> int:
-    if amount > val:
-        result = 100 - (amount - val) % 100
+def compute_left(amount: int, val: int) -> Tuple[int]:
+    if not amount > val:
+        return (val - amount, 0)
 
-        if result == 100:
-            return 0
-        return result
-    else:
-        return val - amount
+    calc = 100 - (amount - val)
+    result = calc % 100
 
+    if result == 100:
+        return 0
 
-def compute_right(amount: int, val: int) -> int:
-    if amount + val > 99:
-        return (amount + val) % 100
+    count = math.ceil(calc / 100)
 
-    else:
-        return val + amount
+    return (result, count)
 
 
-def compute_amount(direction: str, amount: int, val: int) -> int:
+def compute_right(amount: int, val: int) -> Tuple[int]:
+    if not amount + val > 99:
+        return (val + amount, 0)
+
+    calc = amount + val
+    result = calc % 100
+
+    count = math.floor(calc / 100)
+
+    return (result, count)
+
+
+def compute_amount(
+    direction: str, amount: int, val: int, count_ticks: bool = False
+) -> Tuple[int]:
+    count = 0
     if direction == "L":
-        return compute_left(amount, val)
+        (result, calc) = compute_left(amount, val)
 
     if direction == "R":
-        return compute_right(amount, val)
+        (result, calc) = compute_right(amount, val)
+
+    if not count_ticks:
+        calc = 1 if result == 0 else 0
+
+    count += calc
+    return (result, count)
 
 
-def solve(input: List[str]) -> int:
+def solve(input: List[str], count_ticks: bool = False) -> int:
     val = 50
     count = 0
 
     for combination in input:
         (direction, amount) = (combination[:1], int(combination[1:]))
-        val = compute_amount(direction, amount, val)
+        (val1, count1) = compute_amount(direction, amount, val, count_ticks)
+        val = val1
+        count = count + count1
 
-        print(f"Key={combination} Value={val} {'Bug!' if val == 100 else ''}")
-
-        if val == 0:
-            count += 1
+        print(f"(Key={combination} Value={val} Count={count})")
 
     return count
