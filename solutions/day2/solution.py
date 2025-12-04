46780801bbb4cd6a341160573d9be81a751a02fc
AlexMuzzy <alexmuzzy33@gmail.com>
2025-12-04 22:44:33 +0000

    day 2 part 2

--- solutions/day2/solution.py
+++ solutions/day2/solution.py
@@ -15,33 +15,28 @@ def solveDay2(input: str) -> int:
                     print(f"Invalid={x}")
                     sumOfInvalidIds = sumOfInvalidIds + x
 
-    print(sumOfInvalidIds)
     return sumOfInvalidIds
 
 
 def solve_day2_part2(input: str) -> str:
-    sumOfInvalidIds = 0
-    separatedList = input.split(",")
+    sum_of_invalid_ids = 0
+    separated_list = input.split(",")
 
-    for rangeStr in separatedList:
-        (min, max) = (int(rangeStr.split("-")[0]), int(rangeStr.split("-")[1]))
+    for rangeStr in separated_list:
+        (min_value, max_value) = (
+            int(rangeStr.split("-")[0]),
+            int(rangeStr.split("-")[1]),
+        )
 
-        for x in range(min, max + 1):
+        for x in range(min_value, max_value + 1):
             str1 = str(x)
-            # 0/1
-            # 0-1/2-3
-            # 0-2/3-5
-            # 0-3/4-7
-            if len(str1) % 2 == 0:
-                midway = (int)(len(str1) / 2)
-                for idx, _ in enumerate(str1[:midway]):
-                    str2 = str1[0 : idx + 1]
-                    str3 = str1[idx + 1 : (idx * 2) + 2]
-                    if str2 == str3:
-                        print(f"Invalid={x}")
-                        sumOfInvalidIds = sumOfInvalidIds + x
-                    str2 = str1[0 : idx + 1]
-                    str3 = str1[0 : idx + 1]
+            midway = int(len(str1) / 2)
+            for idx, _ in enumerate(str1[:midway]):
+                str2 = str1[0 : idx + 1]
+                str3 = str1[0 : idx + 1] * (len(str1) // len(str2))
+                if str1 == str3:
+                    print(f"Invalid={x}")
+                    sum_of_invalid_ids = sum_of_invalid_ids + x
+                    break
 
-    print(sumOfInvalidIds)
-    return sumOfInvalidIds
+    return sum_of_invalid_ids
